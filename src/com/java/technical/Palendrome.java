014eb98f40f3382e7eb65baf235a6fe8bcc6c04e
Mahesh Babu Neeli <maheshbabu22.neeli@gmail.com>
2026-01-31 07:48:10 +1100

    Integer Palendrome

--- src/com/java/technical/Palendrome.java
+++ src/com/java/technical/Palendrome.java
@@ -4,9 +4,10 @@ public class Palendrome {
 
     public static void main (String[] args) {
 
-        // 12321
-        // 0
-        // 123
+        // 12321   true
+        // 0       true
+        // 123     false
+        //-12321   true
         int input = -12321;
         boolean isPalendrome = verifyPalendrome(input);
         System.out.println("the given number is palendrome "+ isPalendrome) ;
@@ -18,7 +19,7 @@ public class Palendrome {
         int originalInput = input;
         int reversed = 0;
 
-        while (input > 0) {   // 12321
+        while (input != 0) {   // 12321
             int remainder = input % 10;    //1 2
             reversed = reversed * 10 + remainder; // 1 10 +2   // 12
             input = input / 10;  // 1232
